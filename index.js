6dbd3f731539f0081420c4d314364a7a7bd9bcd6
Xuming Zeng <xsznix@gmail.com>
2015-09-26 20:30:05 -0500

    test contextio key/secret env variables

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.set('view engine', 'ejs');
 app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
-	res.render('index');
+	res.render('index', {
+		key: process.env['CONTEXTIO_KEY'],
+		secret: process.env['CONTEXTIO_SECRET']
+	});
 });
 
 var server = app.listen(process.env.port || 3000);
