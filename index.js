6990c038c4c02a1f2c7c16afeb94e640d1e659b3
Xuming Zeng <xsznix@gmail.com>
2015-09-26 21:04:24 -0500

    test connection string

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var ContextIO = require('contextio');
+var cc = new ContextIO.Client({
+	key: process.env['CONTEXTIO_KEY'],
+	secret: process.env['CONTEXTIO_SECRET']
+});
+
+var connectionString = process.env['MYSQLCONNSTR_DefaultConnection'];
+
 var express = require('express');
 var app = express();
 
@@ -5,10 +13,16 @@ app.set('view engine', 'ejs');
 app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
-	res.render('index', {
-		key: process.env['CONTEXTIO_KEY'],
-		secret: process.env['CONTEXTIO_SECRET']
-	});
+	res.render('index');
+});
+
+app.get('/oauth/callback', function (req, res) {
+	// save req.query('contextio_token')
+	res.send('lol');
+});
+
+app.get('/connstr', function (req, res) {
+	res.send(connectionString);
 });
 
 var server = app.listen(process.env.port || 3000);
